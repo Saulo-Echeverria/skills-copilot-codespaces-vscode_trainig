fc055b3ebd6c61ff0c0a74aa85a9ddb928fa083a
Saulo-Echeverria <saulogomes4@hotmail.com>
2023-08-26 12:38:32 +0000

    Mudanças em devcontainer e skills.js

--- skills.js
+++ skills.js
@@ -1,2 +1,8 @@
+<<<<<<< Updated upstream
 skills.js
 function calculateNumbers(var1, var2)
+=======
+function calculateNumbers(var1, var2) {
+    return var1 + var2; 
+}    
+>>>>>>> Stashed changes
